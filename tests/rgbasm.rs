225aa4d82bb4395f61147a4be27dd22f3e1a59c5
ISSOtm <me@eldred.fr>
2025-08-09 12:25:07 +0200

    Update interpolation recursion test data

--- tests/rgbasm.rs
+++ tests/rgbasm.rs
@@ -9,7 +9,7 @@ datatest_stable::harness! {
 }
 
 const RGBASM_PATH: &str = env!("CARGO_BIN_EXE_rgbasm");
-const RGBLINK_PATH: &str = "rgblink";
+const RGBLINK_PATH: &str = "rgblink"; // TODO: use rsgblink when it's complete
 
 const ACTION_ENV_VAR_NAME: &str = "SNAPSHOTS_ASM";
 
