7dfbda854e1f019f8570bb1685c7cf7c634d79ae
ISSOtm <me@eldred.fr>
2025-08-13 19:21:35 +0200

    Pass some conditional tests

--- tests/rgbasm/elif-after-taken-if/test.asm
+++ tests/rgbasm/elif-after-taken-if/test.asm
@@ -1,8 +1,8 @@
 if 1
 	println "taken if"
-elif 2 / 0 ; avoided fatal "Division by zero" error
+elif 2 / 0 ; avoided "Division by zero" error
 	println "untaken elif"
-elif 3 / 0 ; avoided fatal "Division by zero" error
+elif 3 / 0 ; avoided "Division by zero" error
 	println "untaken after untaken"
 endc
 
@@ -10,14 +10,14 @@ if 0
 	println "untaken if"
 elif 1
 	println "taken elif"
-elif !@#$ ; avoided fatal syntax error
+elif !@#$ ; avoided syntax error
 	println "untaken elif"
-elif %^&* ; avoided fatal syntax error
+elif %^&* ; avoided syntax error
 	println "untaken after untaken"
 endc
 
 if 0
 	println "untaken if"
-elif 1 / 0 ; fatal "Division by zero" error
-	println "unreached elif"
+elif 1 / 0
+	println "taken elif"
 endc
