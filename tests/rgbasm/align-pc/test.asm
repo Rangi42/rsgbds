9ca326478d1a10d5ef36779c7cb2ec3bf1bcf9cb
ISSOtm <me@eldred.fr>
2025-08-13 12:50:06 +0200

    Pass `align-pc` test
    Interestingly, the comments were wrong!

--- tests/rgbasm/align-pc/test.asm
+++ tests/rgbasm/align-pc/test.asm
@@ -4,19 +4,19 @@ SECTION "fixed org", SRAM[$BEAD]
 	align 5,$17
 
 
-; Should land at $0001
+; Should land at $0005.
 SECTION "align", ROM0,ALIGN[1,1]
 	db 69
 	align 1 ; This wants to go at $0000, $0002, $0004...
 
 
-; Should land at $0003
+; Should land at $0003.
 SECTION "under-aligned", ROM0,ALIGN[1,1]
 	dw $BEEF
 	align 2,1 ; This wants to go at $0001, $0005, $0009...
 
 
-; Should land at $0005
+; Should land at $0001, since it has the highest alignment.
 SECTION "forced align", ROM0
 	dw $DEAD
 	align 2,3 ; This wants to go at $0003, $0007, $000B...
