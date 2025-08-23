a53f1fdd9ffe8f3c2dab75c970ae7c72ed9c103d
ISSOtm <me@eldred.fr>
2025-08-23 23:32:54 +0200

    Implement symbol definition

--- src/asm/syntax/semantics/symbols.rs
+++ src/asm/syntax/semantics/symbols.rs
@@ -1,7 +1,12 @@
 use compact_str::CompactString;
 use either::Either;
 
-use crate::{diagnostics, expr::Expr, sources::Span, Identifier};
+use crate::{
+    diagnostics,
+    expr::{BinOp, Expr},
+    sources::Span,
+    Identifier,
+};
 
 use super::parse_ctx;
 
@@ -62,6 +67,154 @@ impl parse_ctx!() {
         } else {
             // Likewise, assume treat anything else as a symbol expression; non-numeric symbols will get filtered out at evaluation time.
             Either::Left(Expr::symbol(name, span))
+        }
+    }
+
+    pub fn define_string(
+        &mut self,
+        export: Option<usize>,
+        redef: bool,
+        name: Identifier,
+        (string, _span): (CompactString, Span),
+        span_idx: usize,
+    ) {
+        if let Some(span_idx) = export {
+            let span = &self.line_spans[span_idx];
+            self.error(span, |error| {
+                error.set_message("cannot export string constants");
+                error.add_label(
+                    diagnostics::error_label(span).with_message("consider removing this"),
+                );
+            });
+        }
+
+        let span = self.nth_span(span_idx);
+
+        self.symbols.define_string(
+            name,
+            self.identifiers,
+            span,
+            string,
+            redef,
+            self.sections
+                .active_section
+                .as_ref()
+                .map(|(_data_sect, sym_sect)| sym_sect),
+            self.macro_args.last(),
+            self.nb_errors_left,
+            self.options,
+        );
+    }
+
+    pub fn define_numeric(
+        &mut self,
+        export: Option<usize>,
+        redef: bool,
+        name: Identifier,
+        mutable: bool,
+        expr: Expr,
+        span_idx: usize,
+    ) {
+        match self.try_const_eval(&expr) {
+            Ok((value, _span)) => {
+                let span = self.nth_span(span_idx);
+
+                self.symbols.define_constant(
+                    name,
+                    self.identifiers,
+                    span,
+                    value,
+                    mutable,
+                    export.is_some(),
+                    redef,
+                    self.sections
+                        .active_section
+                        .as_ref()
+                        .map(|(_data_sect, sym_sect)| sym_sect),
+                    self.macro_args.last(),
+                    self.nb_errors_left,
+                    self.options,
+                );
+            }
+            Err(error) => self.report_expr_error(error),
+        }
+    }
+
+    pub fn compound_assignment(
+        &mut self,
+        export: Option<usize>,
+        redef: bool,
+        name: Identifier,
+        operator: BinOp,
+        expr: Expr,
+        op_span_idx: usize,
+        name_span_idx: usize,
+    ) {
+        let name_span = self.nth_span(name_span_idx);
+        let synthetic_expr = Expr::symbol(name, name_span).binary_op(operator, expr, Span::Builtin); // This span is not used.
+
+        match self.try_const_eval(&synthetic_expr) {
+            Ok((value, span)) => {
+                let span = self.nth_span(op_span_idx);
+                self.symbols.define_constant(
+                    name,
+                    self.identifiers,
+                    span,
+                    value,
+                    true, // Mutable.
+                    export.is_some(),
+                    redef,
+                    self.sections
+                        .active_section
+                        .as_ref()
+                        .map(|(_data_sect, sym_sect)| sym_sect),
+                    self.macro_args.last(),
+                    self.nb_errors_left,
+                    self.options,
+                );
+            }
+            Err(error) => self.report_expr_error(error),
+        }
+    }
+
+    pub fn define_rs(
+        &mut self,
+        export: Option<usize>,
+        redef: bool,
+        name: Identifier,
+        stride: i32,
+        maybe_expr: Option<Expr>,
+        span_idx: usize,
+    ) {
+        let offset = match maybe_expr {
+            Some(expr) => self.try_const_eval(&expr).map(|(value, _span)| value),
+            None => Ok(1),
+        };
+        match offset {
+            Ok(value) => {
+                let rs = self.symbols.rs();
+                let constant_value = std::mem::replace(rs, rs.wrapping_add(value * stride));
+
+                let span = self.nth_span(span_idx);
+
+                self.symbols.define_constant(
+                    name,
+                    self.identifiers,
+                    span,
+                    constant_value,
+                    false,
+                    export.is_some(),
+                    redef,
+                    self.sections
+                        .active_section
+                        .as_ref()
+                        .map(|(_data_sect, sym_sect)| sym_sect),
+                    self.macro_args.last(),
+                    self.nb_errors_left,
+                    self.options,
+                );
+            }
+            Err(error) => self.report_expr_error(error),
         }
     }
 }
