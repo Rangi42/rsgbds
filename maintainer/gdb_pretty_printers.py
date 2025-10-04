aca960cbbd3cc62f86d60291eca4b4267e41e395
ISSOtm <me@eldred.fr>
2025-10-04 04:26:19 +0200

    Add typing to GDB pretty-printer
    Makes my LSP happier

--- maintainer/gdb_pretty_printers.py
+++ maintainer/gdb_pretty_printers.py
@@ -1,13 +1,17 @@
 # This file gets embedded directly into the executables, via `#![debugger_visualizer()]` attributes.
 
+from typing import final
 import gdb
 import re
 
 ARRAY_VEC_REGEX = re.compile(r"^(arrayvec::(\w+::)+)ArrayVec<.+>$")
-class ArrayVecPrinter:
+
+
+@final
+class ArrayVecPrinter(gdb.ValuePrinter):
     "Print an arrayvec::arrayvec::ArrayVec"
 
-    def __init__(self, val):
+    def __init__(self, val: gdb.Value):
         self.length = int(val["len"])
         self.array = val["xs"]
 
@@ -17,7 +21,7 @@ class ArrayVecPrinter:
     def children(self):
         for i in range(self.length):
             # Deref the `MaybeUninit` and the `ManuallyDrop` inside.
-            yield f"[{i}]",self.array[i]["value"]["value"]
+            yield f"[{i}]", self.array[i]["value"]["value"]
 
     def num_children(self):
         return self.length
@@ -26,8 +30,10 @@ class ArrayVecPrinter:
     def display_hint():
         return "array"
 
+
+@final
 class ColorGroupPrinter(gdb.ValuePrinter):
-    def __init__(self, val):
+    def __init__(self, val: gdb.Value):
         array = val["colors"]
         self.length = int(array["len"])
         self.array = array["xs"]
@@ -38,7 +44,7 @@ class ColorGroupPrinter(gdb.ValuePrinter):
     def children(self):
         for i in range(self.length):
             # Deref the `MaybeUninit` and the `ManuallyDrop` inside.
-            yield f"[{i}]",self.array[i]["value"]["value"]
+            yield f"[{i}]", self.array[i]["value"]["value"]
 
     def num_children(self):
         return self.length
@@ -47,8 +53,10 @@ class ColorGroupPrinter(gdb.ValuePrinter):
     def display_hint():
         return "array"
 
+
+@final
 class Rgb16Printer(gdb.ValuePrinter):
-    def __init__(self, val):
+    def __init__(self, val: gdb.Value):
         raw = val["__0"]
         self.red = raw & 0x1F
         self.green = raw >> 5 & 0x1F
@@ -58,8 +66,10 @@ class Rgb16Printer(gdb.ValuePrinter):
     def to_string(self):
         return f"Rgb16(r={self.red}, g={self.green}, b={self.blue}, a={self.alpha})"
 
+
+@final
 class BitVecPrinter(gdb.ValuePrinter):
-    def __init__(self, val, storage_type_name):
+    def __init__(self, val: gdb.Value, storage_type_name: str):
         storage_type = gdb.lookup_type(storage_type_name)
         span = val["bitspan"]
         self.length = span["len"]
@@ -75,7 +85,7 @@ class BitVecPrinter(gdb.ValuePrinter):
             storage = self.ptr[i // self.storage_size]
 
             # Only `Lsb0` is supported.
-            yield f"[{i}]",(storage >> bit_idx) & 1
+            yield f"[{i}]", (storage >> bit_idx) & 1
 
     def num_children(self):
         return self.length
@@ -84,20 +94,22 @@ class BitVecPrinter(gdb.ValuePrinter):
     def display_hint():
         return "array"
 
-def rgbds_lookup(val):
+
+def rgbds_lookup(val: gdb.Value):
     lookup_tag = val.type.tag
     if lookup_tag is None:
         return None
 
     if ARRAY_VEC_REGEX.match(lookup_tag):
         return ArrayVecPrinter(val)
-    if "rgbgfx::color_set::ColorSet" == lookup_tag:
+    if lookup_tag == "rgbgfx::color_set::ColorSet":
         return ColorGroupPrinter(val)
-    if "plumers::color::Rgb16" == lookup_tag:
+    if lookup_tag == "plumers::color::Rgb16":
         return Rgb16Printer(val)
-    if "bitvec::vec::BitVec<usize, bitvec::order::Lsb0>" == lookup_tag:
+    if lookup_tag == "bitvec::vec::BitVec<usize, bitvec::order::Lsb0>":
         return BitVecPrinter(val, "usize")
 
     return None
+
 
 gdb.current_objfile().pretty_printers.append(rgbds_lookup)
